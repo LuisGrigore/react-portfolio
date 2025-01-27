e9cdd1303bee0c0040198585ed56e4071d70a6dd
LuisGrigore <luiscristiangrigore11@gmail.com>
2025-01-27 14:47:37 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/**
+ * @copyright
+ * @license Apache-2.0
+ */
+
+import Header from "./components/Header"
+
+
+const App = () => {
+	return (
+		<div>
+			<Header/>
+		</div>
+	)
+}
+
+export default App
